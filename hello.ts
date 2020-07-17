9e23347ea2014e2a4a4774ef29a67ca25dc06e3c
Shashank <you@example.com>
2020-07-17 17:42:29 +0530

    Variables in type script

--- hello.ts
+++ hello.ts
@@ -3,16 +3,26 @@ let message = "Hello Shashank"
 
 let x = 10;
 const y = 20;
-
 let sum ;
 const title = "CodeEvolution" ; 
 let isBegginer: boolean = true;
 let total: number = 23;
 let name: string = "Neha"
+
 let senetence:string = `My Name is ${name}
 And I am a Beginner TypeScript Developer`
 
 console.log(isBegginer);
 
 console.log(senetence)
+
+let n:null = null;
+let u:undefined = undefined;
+console.log(n)
+
+let isNew:boolean = null;
+let myName:string = undefined;
+
+console.log(myName)
+
 
